caf2de50fa1dc7faa7e52732038222c1043585f2
Alaamer <ahmedmuhmmed239@gmail.com>
2024-04-08 11:35:01 +0200

    update

---
+++ README.md
@@ -0,0 +1,17 @@
+# sentry demo project
+
+this project is just a demo of how to use sentry or how it looks to be using sentry and how the error detection works
+
+anyway we have seen how to use sentry and how it looks to be using sentry and how the error detection works
+
+sentry has a trial of 14 days and for now we dont need sentry at all. also it provides a functions to be called when an error happens.
+
+so becuase of this we wont go further with this project
+this is kinda sufficient for now
+
+Note: when i init the sentry at setup.py it didnt work but when i init in main.py it worked it looks like its need to be in the main app file [main.py]
+
+also sentry provide configured sdk for django and flask and other frameworks it has many frameworks with python specially.
+
+here is a link of playlist about python sentry
+https://www.youtube.com/playlist?list=PLOwEowqdeNMr7wwxSeUChfvxkcvOD4PhW
---
+++ main.py
@@ -0,0 +1,32 @@
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://231a6460234cea42fb7d3e823140d1be@o4507036129361920.ingest.us.sentry.io/4507036145090560",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
+
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+division_by_zero = 1 / 0
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
